521ba73e4ea36973b9ec041ac5b5794f3b82036d
shaleentn <shaleentn@gmail.com>
2023-08-21 12:26:35 -0700

    Creating a customized printf

---
+++ print1.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+/**
+  *_printf - produces output according to a format
+  *@format: pointer to the string to be output
+  *Return: number of characters to be printed
+  */
+int _printf(const char *format, ...)
+{
+	int i, j, m;
+	char *string;
+	char c;
+	va_list l;
+
+	m = strlen(format);
+
+	va_start(l, format);
+
+	j = 0;
+
+	for (i = 0; i < m; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				c = va_arg(l, int);
+				j += printf("%c", c);
+			}
+			else if (format[i] == 's')
+			{
+				string = va_arg(l, char *);
+				if (string == NULL)
+					printf("nil");
+				j += printf("%s", string);
+			}
+			else if (format[i] == '%')
+				j += printf("%%");
+		}
+		else
+			j += putchar(format[i]);
+	}
+	va_end(l);
+	return (j);
+}
