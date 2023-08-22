859209f57afe1cd10c8cc26fac47ed51194e2114
victortimbwa <victortimbwa@gmail.com>
2023-08-23 00:50:44 +0300

    updated task 1

--- main.h
+++ main.h
@@ -1,11 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+int printf_int(va_list args);
+int printf_dec(va_list args);
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H*/
