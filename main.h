2f247efdb5461561b639b928fda81cc607f927c3
victortimbwa <victortimbwa@gmail.com>
2023-08-22 12:27:40 +0300

    Updated print1.h

--- main.h
+++ main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /***************FUNCTION producing output formated**********/
 
 int _printf(const char *format, ...);
